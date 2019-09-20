ef4810365fb8bc63e098c562cc7e47ff151cb3c9
Hali <hgabor@level14.hu>
2019-09-20 10:45:02 +0200

    Szin modositas

--- PetrikGitKozos/Program.cs
+++ PetrikGitKozos/Program.cs
@@ -11,7 +11,7 @@ namespace PetrikGitKozos
     {
         static void Main(string[] args)
         {
-            Color szin = Color.Yellow;
+            Color szin = Color.AliceBlue;
             string szoveg = "Surprise!";
 
             Form1 f = new Form1(szin, szoveg);
